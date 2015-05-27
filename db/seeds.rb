b823e7786fbf715be8460ae2f3d3c755eccd4f3a
Rodrigo Torres <rtdesigns@yahoo.com>
2015-05-27 15:57:33 -0500

    movie data seeded and user and events controllers made

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
-filename = File.expand_path('../Movies_AT_Park.csv', __FILE__)
+# filename = File.expand_path('../Movies_AT_Park.csv', __FILE__)
+#
+# CSV.foreach(filename, :headers=>true) do |csv_obj|
+#
+#   Event.create!({
+#      title: csv_obj[' EventName '],
+#      moviename: csv_obj[' MovieName'],
+#      hour: csv_obj[' StartDate'],
+#      link: csv_obj[' EventUrl'],
+#      movierating: csv_obj[' MovieRating'],
+#      location: csv_obj['Location 1']
+#      })
+#
+# end
+
+
+filename = File.expand_path('../Chicago_Festivals.csv', __FILE__)
 
 CSV.foreach(filename, :headers=>true) do |csv_obj|
 
   Event.create!({
-     title: csv_obj[' EventName '],
-     moviename: csv_obj[' MovieName'],
-     hour: csv_obj[' StartDate'],
-     link: csv_obj[' EventUrl'],
-     movierating: csv_obj[' MovieRating'],
-     location: csv_obj['Location 1']
+
+     category: csv_obj['Category'],
+     title: csv_obj['Title'],
+     description: csv_obj['Description'],
+     startdate: csv_obj['Startdate'],
+     hour: csv_obj['Times'],
+     link: csv_obj['EventUrl'],
+     neighborhood: csv_obj['Neighborhood'],
+     location: csv_obj['Location'],
+     image: csv_obj['imageURl']
+
      })
 
 end
