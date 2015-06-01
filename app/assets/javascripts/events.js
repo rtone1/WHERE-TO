7e2659bf0a7bda6de8e913ebac4475db971c81a2
Rodrigo Torres <rtdesigns@yahoo.com>
2015-06-01 11:18:40 -0500

    logo.png added and events erb styled

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,19 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var app = {};
+var app =  {};
 
 //Backbone model for Events
 app.EventModel = Backbone.Model.extend();
 
 //Backbone template Events
-// app.EventTemplate = $('#event-template').html();
+app.EventTemplate = $('#eventtemplate').html();
 
 //Backbone view for Events
 app.EventView = Backbone.View.extend({
 
   tagName: 'div',
   className: 'eventInfo',
-  template: _.template('<div class="imagePort"><img src="<%= image %>" /></div><section id="infosection"><h2> <%= title %> </h2></section>'),
+  //template: _.template(app.EventTemplate),
+  template: _.template('<div class="imagePort" style="background-image: url(<%= image %>)"><div class="image-haeder"> <h1> <%= title %> </h1><div class="rating"><h3> <%= moviename %> </h3> <b> <%= movierating %> </b></div> </div> </div> <section id="infosection"> <p class="dates"> <strong> <%= startdate %> </strong> <br> <%= hour %> </p> <div class="location"> <h4> <%= neighborhood %> </h4>  <%= location %> </div> <p class="descrip"> <%= description %> </p> <a href=" <%= link %> " target="_blank"> <%= link %> </a> </section>'),
   intialize: function(){
       this.listenTo(this.model, "change", this.render);
   },
@@ -27,9 +28,9 @@ app.EventView = Backbone.View.extend({
 });//end of EventView
 
 // Date varible in month/day/year format
-var now = moment().format("MMM Do YYYY");
+app.now = moment().format("MMM Do YYYY");
 // Date varible in number format
-var now2 = moment().format("L");
+app.now2 = moment().format("L");
 
 
 $(document).ready(function(){
@@ -90,6 +91,8 @@ function getFestivals(){
 
 };
 
+
+// click events for displaying events
 var display = $('.content');
 
 $('#allEvents').on('click', function(){
